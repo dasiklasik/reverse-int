31aee6bc36e40fb4c0e4f3cfb4a2ad652510ea72
dasiklasik <daryasamsonowicz@gmail.com>
2020-10-13 12:11:36 +0300

    init: start and do reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let string = n.toString();
+    if (string[0] === '-') {
+        string = string.slice(1, string.length);
+        n = string.split('').reverse().join('');
+        return Number(n);
+    }
+
+    n = n.toString().split('').reverse().join('');
+    return Number(n);
 }
